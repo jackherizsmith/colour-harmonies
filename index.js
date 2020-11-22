1c207a6e5418aa4429859ca312917f0fdcdfba43
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-11-22 01:10:49 +0000

    added sections, css into grid ready for components

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ inputs.forEach((input) => {
   });
 });
 
-let height = document.body.offsetHeight,
-  width = document.body.offsetWidth / 3;
+const colorBox = document.querySelector(".colour-box");
+
+let height = colorBox.offsetHeight,
+  width = colorBox.offsetWidth;
 
 window.addEventListener("resize", () => {
-  height = document.body.offsetHeight;
-  width = document.body.offsetWidth / 3;
+  height = colorBox.offsetHeight;
+  width = colorBox.offsetWidth;
+  console.log(height, width);
 });
 
 const harmonies = { split: 30, triad: 60, analogous: 150 };
@@ -40,9 +43,9 @@ function setHslText(hue, saturation, lightness) {
   lightText.textContent = lightness + "%";
 }
 
-const elementA = document.getElementsByClassName("colour--a");
-const elementB = document.getElementsByClassName("colour--b");
-const elementC = document.getElementsByClassName("colour--c");
+const elementA = document.getElementsByClassName("colour--main");
+const elementB = document.getElementsByClassName("colour--dark");
+const elementC = document.getElementsByClassName("colour--light");
 
 const hueGradient = document.querySelector(".hue-gradient");
 const lightGradient = document.querySelector(".light-gradient");
@@ -50,59 +53,66 @@ const lightGradient = document.querySelector(".light-gradient");
 let xToHue = 0,
   saturation = 60,
   yToLight = 0,
-  hsls;
+  hsls,
+  updating = true;
 
 function updateColour() {
-  const [hueB, hueC] = setHue(xToHue);
+  if (updating) {
+    const [hueB, hueC] = setHue(xToHue);
 
-  hslA = `hsl(${xToHue}, ${saturation}%, ${yToLight}%)`;
-  hslB = `hsl(${hueB}, ${saturation}%, ${yToLight}%)`;
-  hslC = `hsl(${hueC}, ${saturation}%, ${yToLight}%)`;
-  hsls = `body {
-  --a: ${hslA};
-  --b: ${hslB};
-  --c: ${hslC};
-}`;
+    hslA = `hsl(${xToHue}, ${saturation}%, ${yToLight}%)`;
+    hslB = `hsl(${hueB}, ${saturation}%, ${100 - yToLight}%)`;
+    hslC = `hsl(${hueC}, ${saturation}%, ${yToLight}%)`;
+    hsls = `body {
+    --a: ${hslA};
+    --b: ${hslB};
+    --c: ${hslC};
+  }`;
 
-  Array.from(elementA).forEach((element) => {
-    element.style.background = hslA;
-  });
-  Array.from(elementB).forEach((element) => {
-    element.style.background = hslB;
-  });
-  Array.from(elementC).forEach((element) => {
-    element.style.background = hslC;
-  });
-  hueGradient.style.background = `linear-gradient(
-    90deg,
-    hsl(0, ${saturation}%, ${yToLight}%) 0%,
-    hsl(36, ${saturation}%, ${yToLight}%) 10%,
-    hsl(72, ${saturation}%, ${yToLight}%) 20%,
-    hsl(108, ${saturation}%, ${yToLight}%) 30%,
-    hsl(144, ${saturation}%, ${yToLight}%) 40%,
-    hsl(180, ${saturation}%, ${yToLight}%) 50%,
-    hsl(216, ${saturation}%, ${yToLight}%) 60%,
-    hsl(252, ${saturation}%, ${yToLight}%) 70%,
-    hsl(288, ${saturation}%, ${yToLight}%) 80%,
-    hsl(324, ${saturation}%, ${yToLight}%) 90%,
-    hsl(360, ${saturation}%, ${yToLight}%) 100%
-  )`;
-  lightGradient.style.background = `linear-gradient(
-    0deg, 
-    hsl(${xToHue}, ${saturation}%, 100%) 0%, 
-    hsl(${xToHue}, ${saturation}%, 50%) 50%, 
-    hsl(${xToHue}, ${saturation}%, 0%) 100%)`;
-  setHslText(xToHue, saturation, yToLight);
+    Array.from(elementA).forEach((element) => {
+      element.style.background = hslA;
+    });
+    Array.from(elementB).forEach((element) => {
+      element.style.background = hslB;
+    });
+    Array.from(elementC).forEach((element) => {
+      element.style.background = hslC;
+    });
+    hueGradient.style.background = `linear-gradient(
+      90deg,
+      hsl(0, ${saturation}%, ${yToLight}%) 0%,
+      hsl(36, ${saturation}%, ${yToLight}%) 10%,
+      hsl(72, ${saturation}%, ${yToLight}%) 20%,
+      hsl(108, ${saturation}%, ${yToLight}%) 30%,
+      hsl(144, ${saturation}%, ${yToLight}%) 40%,
+      hsl(180, ${saturation}%, ${yToLight}%) 50%,
+      hsl(216, ${saturation}%, ${yToLight}%) 60%,
+      hsl(252, ${saturation}%, ${yToLight}%) 70%,
+      hsl(288, ${saturation}%, ${yToLight}%) 80%,
+      hsl(324, ${saturation}%, ${yToLight}%) 90%,
+      hsl(360, ${saturation}%, ${yToLight}%) 100%
+    )`;
+    lightGradient.style.background = `linear-gradient(
+      0deg, 
+      hsl(${xToHue}, ${saturation}%, 100%) 0%, 
+      hsl(${xToHue}, ${saturation}%, 50%) 50%, 
+      hsl(${xToHue}, ${saturation}%, 0%) 100%)`;
+    setHslText(xToHue, saturation, yToLight);
+  }
 }
 
-window.addEventListener("mousemove", (event) => {
-  xToHue = Math.floor((event.clientX / width) * 120);
-  yToLight = Math.floor((event.clientY / height) * 100);
-  updateColour();
-  const tooLight = yToLight > 52;
-  document.querySelector(".hsl-text--hue").style.color = tooLight
-    ? "black"
-    : "white";
+const textOnMain = Array.from(document.getElementsByClassName("text-on-main"));
+
+colorBox.addEventListener("mousemove", (event) => {
+  if (updating) {
+    xToHue = Math.round((event.clientX / width) * 360);
+    yToLight = Math.round((event.clientY / height) * 100);
+    updateColour();
+    const tooLight = yToLight > 58;
+    textOnMain.forEach(
+      (element) => (element.style.color = tooLight ? "black" : "white")
+    );
+  }
 });
 
 const labels = document.querySelectorAll("label");
@@ -112,13 +122,11 @@ const copied = document.querySelector(".copied");
 
 function setSat(key) {
   saturation =
-    key === "ArrowUp"
-      ? Math.min(saturation + 1, 100)
-      : Math.max(saturation - 1, 0);
+    key === "a" ? Math.min(saturation + 1, 100) : Math.max(saturation - 1, 0);
   satText.textContent = saturation + "%";
 }
 
-const keys = ["ArrowUp", "ArrowDown"];
+const keys = ["a", "z"];
 const keyArrows = document.querySelectorAll(".keys");
 
 window.addEventListener("keydown", (event) => {
@@ -141,6 +149,10 @@ window.addEventListener("keydown", (event) => {
     cssVars.select();
     document.execCommand("copy");
     document.body.removeChild(cssVars);
+  }
+
+  if (event.key === " ") {
+    updating = !updating;
   }
 });
 
